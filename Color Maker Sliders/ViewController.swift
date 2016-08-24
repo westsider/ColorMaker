16e8c16dbb7c7227538b8d03ac9ec63e8120fefd
Warren Hansen <whansen1@me.com>
2016-08-24 14:08:21 -0700

    1st load of color mixer

--- Color Maker Sliders/ViewController.swift
+++ Color Maker Sliders/ViewController.swift
@@ -9,17 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var myView: UIView!
+    
+    @IBOutlet weak var redSlider: UISlider!
+    
+    @IBOutlet weak var greenSlider: UISlider!
+    
+    @IBOutlet weak var blueSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // custom thumb color
+        redSlider.thumbTintColor = UIColor.redColor()
+        greenSlider.thumbTintColor = UIColor.greenColor()
+        blueSlider.thumbTintColor = UIColor.blueColor()
+        
+        // set targets for sliders
+        redSlider.addTarget(self, action: #selector(ViewController.changeColorComponent), forControlEvents: UIControlEvents.ValueChanged)
+        
+        greenSlider.addTarget(self, action: #selector(ViewController.changeColorComponent), forControlEvents: UIControlEvents.ValueChanged)
+        
+        blueSlider.addTarget(self, action: #selector(ViewController.changeColorComponent), forControlEvents: UIControlEvents.ValueChanged)
+        
+        // Set colorView on launch
+        changeColorComponent()
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func changeColorComponent() {
+        
+        // Make sure the program doesn't crash if the controls aren't connected
+        if self.redSlider == nil {
+            return
+        }
+        // adjust colr mix with sliders
+        let r: CGFloat = CGFloat(self.redSlider.value)
+        let g: CGFloat = CGFloat(self.greenSlider.value)
+        let b: CGFloat = CGFloat(self.blueSlider.value)
+        
+        myView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
     }
-
-
 }
-
